7924815e628e8f35bde1e9afba49ecc7779624ff
Mahima-2004 <mahimagowda60@gmail.com>
2025-12-16 14:23:26 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -81,7 +81,7 @@ st.set_page_config(page_title="Vehicle Breakdown Predictor - Integrated", layout
 
 # ----------------- CONFIG PATHS -----------------
 PROJECT_ROOT = Path(".")
-MODEL_PATH = PROJECT_ROOT / "saved_models" / "rf_pipeline.pkl"
+MODEL_PATH = PROJECT_ROOT / "rf_pipeline.pkl"
 DATA_PATH = PROJECT_ROOT / "vehicle_breakdown_prediction_dataset.xlsx"
 USERS_CSV = PROJECT_ROOT / "users.csv"
 FEEDBACK_CSV = PROJECT_ROOT / "feedback.csv"
@@ -892,3 +892,4 @@ if admin_idx is not None:
 # ----------------- Footer -----------------
 st.markdown("---")
 st.caption("Notes: Passwords are hashed before storage. For production, use a proper DB and hosted auth (Firebase/Auth0). Keep Twilio and other secrets in environment variables.")
+
